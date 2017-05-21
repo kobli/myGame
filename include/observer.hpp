8a243e6748fc788702f20e80e1e7f64b94fd8793
Ales Koblizek <koblizek.ales@seznam.cz>
2017-05-21 13:38:34 +0200

    WIP - make sure observ/er/able/abler can be moved
    this should make it possible for example to have a vector of observers
    
    TODO:
    - do not call observabler::onRemove from Observable base destructor - the
    derived part of the object does not exist anymore and the dynamic cast
    fails
    - more tests

--- include/observer.hpp
+++ include/observer.hpp
@@ -1,157 +1,220 @@
+#ifndef OBSERVER_HPP_16_11_12_10_26_41
+#define OBSERVER_HPP_16_11_12_10_26_41 
 #include <vector>
 #include <list>
 #include <memory>
 #include <algorithm>
+#include <iostream>
+#include "reallocable.hpp"
+#include <exception>
 
-#ifndef OBSERVER_HPP_16_11_12_10_26_41
-#define OBSERVER_HPP_16_11_12_10_26_41 
+#include <typeinfo>
 
-template <typename messageT>
-class Observer;
-
-template <typename messageT>
-class Observable;
 
 template <typename messageT>
-class Observer {
-	friend Observable<messageT>;
+class Observer_;
+
+template <typename msgT>
+using Observer = Reallocable<Observer_<msgT>>;
+
+template <typename messageT>
+class Observable_;
+
+template <typename msgT>
+using Observable = Reallocable<Observable_<msgT>>;
+
+
+template <typename messageT>
+class Observer_ {
+	//TODO try to hide onObservable... methods .. in Observabler too
+	//friend Observable_;
+	//protected:
 	public:
-		Observer(): _self{this, [](Observer<messageT>*){}} {
-		}
-		virtual void observe(Observable<messageT>& o) {
-			o.addObserver(*this);
-		}
 		// called when the observer is starting to observe an object
-		virtual void onObservableAdd(messageT& m) = 0;
+		virtual void onObservableAdd(Observable_<messageT>& o, const messageT& m) = 0;
 		// called when the observable is changed
-		virtual void onObservableUpdate(messageT& m) = 0;
+		virtual void onObservableUpdate(Observable_<messageT>& o, const messageT& m) = 0;
 		// called when the observable stops observing an object
 		// - either because the observable was destroyed or 
 		// because Observable::removeObserver was called
-		virtual void onObservableRemove(messageT& m) = 0;
-		virtual void onObservableRemove2(Observable<messageT>*, messageT& m) { onObservableRemove(m); }
+		virtual void onObservableRemove(Observable_<messageT>& o, const messageT& m) = 0;
 		// i guess observable.remove(self) should not be called in dtor
-		virtual ~Observer()
+		virtual ~Observer_()
 		{}
-	private:
-		std::shared_ptr<Observer<messageT>> _self;
+
+		virtual void swap(Observer_<messageT>&) {
+		}
 };
 
 template <typename messageT>
-class Observable {
-	friend Observer<messageT>;
+class Observable_ {
 	public:
-		Observable(messageT obsAddMsg, messageT obsRemMsg): _obsAddMsg{obsAddMsg}, _obsRemMsg{obsRemMsg}
+		Observable_(messageT obsAddMsg, messageT obsRemMsg): _obsAddMsg{obsAddMsg}, _obsRemMsg{obsRemMsg}, _sendAddRemMsg(true)
 		{}
 
-	protected:
-		virtual void addObserver(Observer<messageT>& obs)
-		{
-			_observers.push_back(obs._self);
+		Observable_(): _sendAddRemMsg(false)
+		{}
+
+		Observable_(Observable_& other) noexcept:
+			_observers{other._observers},
+			_obsAddMsg{other._obsAddMsg},
+			_obsRemMsg{other._obsRemMsg},
+		 _sendAddRemMsg{other._sendAddRemMsg}	{
+		}
+
+		Observable_(Observable_&& other) noexcept {
+			swap(*this, other);
+		}
+
+		Observable_& operator=(Observable_ other) noexcept {
+			swap(*this, other);
+			return *this;
+		}
+
+		void swap(Observable_& other) noexcept { //TODO when is this really noexcept?
+			using std::swap;
+			swap(_observers, other._observers);
+			swap(_obsAddMsg, other._obsAddMsg);
+			swap(_obsRemMsg, other._obsRemMsg);
+			swap(_sendAddRemMsg, other._sendAddRemMsg);
+		}
+
+		void notifyObservers(const messageT& m) {
+			for(int i = 0; i < _observers.size(); i++) {
+				auto& observer = _observers[i];
+				if(auto ospt = observer.lock())
+					(*ospt)->onObservableUpdate(*this, m);
+				else {
+					_observers.erase(_observers.begin()+i);
+					i--;
+				}
+			}
+		}
+
+		virtual ~Observable_() {
+			std::cout << "observable dtor\n";
+			for(auto& o : _observers) {
+				if(auto spt = o.lock())
+					sendRemMsg(**spt);
+			}
+		}
+
+		virtual void addObserver(Observer<messageT>& obs) {
+			_observers.push_back(obs.getSelf());
 			sendAddMsg(obs);
 		}
 
-		//TODO make removeObserver private - same as addObserver
-	public:
-		virtual void sendAddMsg(Observer<messageT>& obs)
-		{
-			obs.onObservableAdd(_obsAddMsg);
-		}
-
-		void removeObserver(Observer<messageT>& obs)
-		{
-			auto it = _observers.begin();
-			for(; it != _observers.end(); it++)
-				if(it->get() == &obs)
-					break;
-			if(it != _observers.end())
-			{
-				if(auto spt = it->lock())
-					spt->onObservableRemove2(this, _obsRemMsg);
-				_observers.erase(it);
+		void removeObserver(Observer<messageT>& obs) {
+			for(auto it = _observers.begin(); it != _observers.end(); it++) {
+				if(auto spt = it->lock()) {
+					if(*spt.get() == &obs) {
+						sendRemMsg(**spt);
+						_observers.erase(it);
+						break;
+					}
+				}
 			}
 		}
 
-		void notifyObservers(messageT& m)
-		{
-			for(auto& o : _observers)
-			{
-				if(auto spt = o.lock())
-					spt->onObservableUpdate(m);
-			}
+		virtual void sendAddMsg(Observer<messageT>& observer) {
+			if(_sendAddRemMsg)
+				observer.onObservableAdd(*this, _obsAddMsg);
 		}
 
-		void removeDeadObservers()
-		{
-			_observers.erase(std::remove_if(_observers.begin(), 
-						_observers.end(),
-						[](std::weak_ptr<Observer<messageT>> p){return !p.lock();}),
-					_observers.end());
+		virtual void sendRemMsg(Observer<messageT>& observer) {
+			std::cout << "observable sendRemMsg\n";
+			if(_sendAddRemMsg)
+				observer.onObservableRemove(*this, _obsRemMsg);
 		}
 
-		virtual ~Observable()
-		{
-			for(auto& o : _observers)
-			{
-				if(auto spt = o.lock())
-					spt->onObservableRemove2(this, _obsRemMsg);
-			}
-		}
 
 	private:
-		std::vector<std::weak_ptr<Observer<messageT>>> _observers;
+		std::vector<std::weak_ptr<Observer<messageT>*>> _observers;
 		messageT _obsAddMsg;
 		messageT _obsRemMsg;
+		bool _sendAddRemMsg;
 };
 
-// an observable observer
 template <typename messageT>
-class Observabler: public Observable<messageT>, protected Observer<messageT> {
+void swap(Observable_<messageT>& lhs, Observable_<messageT>& rhs) {
+	lhs.swap(rhs);
+}
+
+// an observable observer
+// it creates no messages - only forwards whatever it receives up the tree
+//TODO revise: after calling observabler::addObserver the observer should receive addMsgs from all observed objs
+template <typename messageT>
+class Observabler: public Observable<messageT>, public Observer<messageT> {
 	public:
-		Observabler(messageT obsAddMsg, messageT obsRemMsg): Observable<messageT>(obsAddMsg, obsRemMsg)
-		{}
-		virtual ~Observabler()
-		{
-		}
-		virtual void observe(Observable<messageT>& o) {
-			Observer<messageT>::observe(o);
-			_observed.push_back(&o);
-		}
-		//TODO
-		/*
-		virtual void stop_observe(Observable<messageT>& o) {
-			o.addObserver(*this);
-		}
-		*/
-
-		virtual void sendAddMsg(Observer<messageT>& obs)
-		{
-			for(auto o : _observed)
-				o->sendAddMsg(obs);
+		Observabler(messageT obsAddMsg, messageT obsRemMsg): Observable<messageT>(obsAddMsg, obsRemMsg) {
 		}
 
+		Observabler() {
+		}
+
+		Observabler(Observabler&& other) {
+			swap(other);
+		}
+
+		void swap(Observabler& other) {
+			using std::swap;
+			swap(_observed, other._observed);
+			Observable<messageT>::swap(other);
+			Observer<messageT>::swap(other);
+		}
+
+		// when observabler dies, send remMsgs ... but when it reallocates?
+		virtual ~Observabler() {
+			while(!_observed.empty()) {
+				auto& o = _observed.front();
+				if(auto spt = o.lock())
+					(*spt)->sendRemMsg(*this);
+			}
+		}
+
+		// when new observer starts observing, send him addMessages from all observed objects
+		virtual void sendAddMsg(Observer<messageT>& observer) {
+			for(auto& o : _observed)
+				if(auto spt = o.lock())
+					(*spt)->sendAddMsg(observer);
+		}
+
+		virtual void sendRemMsg(Observer<messageT>& observer) {
+			for(auto& o : _observed)
+				if(auto spt = o.lock())
+					(*spt)->sendRemMsg(observer);
+		}
 
 	private:
-		virtual void onObservableAdd(messageT& m)
-		{
+		virtual void onObservableAdd(Observable_<messageT>& o, const messageT& m) {
+			// this method is called when the observabler starts observing an observable
+			this->notifyObservers(m);
+			auto& ro = dynamic_cast<Observable<messageT>&>(o);
+			_observed.push_back(ro.getSelf());
+		}
+
+		virtual void onObservableUpdate(Observable_<messageT>&, const messageT& m) {
 			this->notifyObservers(m);
 		}
 
-		virtual void onObservableUpdate(messageT& m)
-		{
+		//TODO when this is called from Observable_ dtor, the derived part does not exist
+		//anymore and the cast will fail ...
+		//how about if Realocable::self returned ptr to base type?
+		virtual void onObservableRemove(Observable_<messageT>& o, const messageT& m) {
+			// this method is called when the observabler stops observing an observable
+			std::cout << "observabler::onObservableRemove\n";
 			this->notifyObservers(m);
+			std::cout << typeid(o).name() << std::endl;
+			auto& ro = dynamic_cast<Observable<messageT>&>(o);
+			std::cout << "observabler::onObservableRemove casting passed\n";
+			_observed.remove_if([&ro](auto& v) -> bool {
+				if(v.expired())
+					return true;
+				auto vsp = v.lock();
+				return *vsp == &ro;
+			});
 		}
 
-		virtual void onObservableRemove(messageT& m)
-		{
-			this->notifyObservers(m);
-		}
-
-		virtual void onObservableRemove2(Observable<messageT>* o, messageT& m)
-		{
-			onObservableRemove(m);
-			_observed.remove(o);
-		}
-		std::list<Observable<messageT>*> _observed; // TODO are all observed elements always valid? do they stay alive? -- now probably yes
+		std::list<std::weak_ptr<Observable<messageT>*>> _observed;
 };
 #endif /* OBSERVER_HPP_16_11_12_10_26_41 */
--- testsrc/test_observer.cpp
+++ testsrc/test_observer.cpp
@@ -1,87 +1,227 @@
 #include <observer.hpp>
 #include "gtest/gtest.h"
+#include <queue>
 using namespace std;
-/*
-TODO automatic tests
 class Msg
 {
 	public:
-		Msg(bool add=false, bool rem=false): _add{add}, _rem{rem}
+		Msg(int data = 0, bool add=false, bool rem=false): _data{data}, _add{add}, _rem{rem}
 		{}
 
+		bool operator==(const Msg& other) const {
+			return _data == other._data && _add == other._add && _rem == other._rem;
+		}
+
+		int _data;
 		bool _add;
 		bool _rem;
 };
 
-class ObserverImpl: public Observer<Msg>
+ostream& operator<<(ostream& o, const Msg& m) {
+	o << m._data << " " << m._add << " " << m._rem << endl;
+	return o;
+}
+
+class ObserverMock: public Observer<Msg>
 {
-		void onObservableAdd(Msg& m)
-		{
-			cout << "OnOAdd\n";
-			EXPECT_EQ(true, m._add);
+	public:
+		typedef std::queue<Msg> MessageSequence;
+		typedef MessageSequence::container_type MsgSeqCont;
+		ObserverMock(MsgSeqCont s = MsgSeqCont()) : _s{s} {
 		}
 
-		void onObservableUpdate(Msg& m)
-		{
-			cout << "OnOUpd\n";
+		ObserverMock(ObserverMock&) = delete;
+		ObserverMock(ObserverMock&&) = delete; //unnecessary
+		ObserverMock& operator=(ObserverMock&& other) {
+			Observer<Msg>::operator=(std::move(other));
+			std::swap(_s, other._s);
+			return *this;
 		}
 
-		void onObservableRemove(Msg& m)
-		{
-			cout << "OnORem\n";
-			EXPECT_EQ(true, m._rem);
+		~ObserverMock() {
+			EXPECT_EQ(_s.empty(), true);
+		}
+
+		MessageSequence _s;
+	private:
+
+		virtual void onObservableAdd(Observable_<Msg>&, const Msg& m) {
+			onMsg(m);
+		}
+
+		virtual void onObservableUpdate(Observable_<Msg>&, const Msg& m) {
+			onMsg(m);
+		}
+
+		virtual void onObservableRemove(Observable_<Msg>&, const Msg& m) {
+			onMsg(m);
+		}
+
+		void onMsg(const Msg& m) {
+			cout << m;
+			ASSERT_EQ(_s.empty(), false);
+			ASSERT_EQ(m, _s.front());
+			_s.pop();
 		}
 };
 
-TEST(Observer, AddAndUpdate) {
-	Observable<Msg> oa(Msg(true), Msg(false, true));
-	ObserverImpl o;
-	Msg m;
+typedef ObserverMock::MsgSeqCont MsgSeq;
 
-	o.observe(oa);
-	oa.notifyObservers(m);
+TEST(Observer, ObservableAdded) {
+	Observable<Msg> observable(Msg(0, true), Msg(0, false, true));
+	ObserverMock observer(MsgSeq{Msg(0, true)});
+
+	observable.addObserver(observer);
 }
 
-TEST(Observer, UpdateAfterDestr) {
-	Observable<Msg> oa(Msg(true), Msg(false, true));
-	Msg m;
+TEST(Observer, ObservableUpdated) {
+	Observable<Msg> observable(Msg(0, true), Msg(0, false, true));
+	ObserverMock observer(MsgSeq{
+			Msg(0, true),
+			Msg(5),
+			});
+
+	observable.addObserver(observer);
+	observable.notifyObservers(Msg(5));
+}
+
+TEST(Observer, ObservableDeath) {
+	ObserverMock observer(MsgSeq{
+			Msg(0, true),
+			Msg(0, false, true),
+			});
 	{
-		ObserverImpl o;
-
-		o.observe(oa);
+		Observable<Msg> observable(Msg(0, true), Msg(0, false, true));
+		observable.addObserver(observer);
 	}
-	oa.notifyObservers(m);
 }
 
-TEST(Observer, ObservableDies) {
-	ObserverImpl o;
-	Observable<Msg> oa1(Msg(true), Msg(false, true));
-	o.observe(oa1);
-	Msg m;
+TEST(Observer, ObservableRemove) {
+	ObserverMock observer(MsgSeq{
+			Msg(0, true),
+			Msg(0),
+			Msg(0, false, true),
+			});
+	Observable<Msg> observable(Msg(0, true), Msg(0, false, true));
+	observable.addObserver(observer);
+	observable.notifyObservers(Msg(0));
+	observable.removeObserver(observer);
+	observable.notifyObservers(Msg(0));
+}
+
+TEST(Observer, ObserverMove) {
+	// make sure observer dies AFTER observable, so it receives the onRemove message
+	ObserverMock observer2;
 	{
-		cout << "start of block\n";
-		Observable<Msg> oa2(Msg(true), Msg(false, true));
-		o.observe(oa2);
-		cout << "end of block\n";
+		Observable<Msg> observable(Msg(0, true), Msg(0, false, true));
+		{
+			ObserverMock observer(MsgSeq{
+					Msg(0, true),
+					});
+			observable.addObserver(observer);
+			observer2 = std::move(observer);
+		}
+		observer2._s.push(Msg(0));
+		observer2._s.push(Msg(0, false, true));
+		observable.notifyObservers(Msg(0));
 	}
-	cout << "end of func\n";
 }
 
-TEST(Observer, ObserverDies) {
-	Observable<Msg> oa1(Msg(true), Msg(false, true));
-	ObserverImpl o;
-	o.observe(oa1);
-	Msg m;
+TEST(Observer, ObservableUpdateAfterObserverDead) {
+	Observable<Msg> observable(Msg(0, true), Msg(0, false, true));
 	{
-		cout << "start of block\n";
-		ObserverImpl o;
-		o.observe(oa1);
-		cout << "oa - two os updated\n";
-		oa1.notifyObservers(m);
-		cout << "end of block\n";
+		ObserverMock observer(MsgSeq{
+				Msg(0, true),
+				});
+		observable.addObserver(observer);
 	}
-	cout << "oa - only one o updated\n";
-	oa1.notifyObservers(m);
-	cout << "end of func\n";
+	// this shouldn't crash the program
+	observable.notifyObservers(Msg(0)); 
 }
-*/
+
+TEST(Observabler, create) {
+	Observabler<Msg> observabler(Msg(0, true), Msg(0, false, true));
+}
+
+TEST(Observabler, ObserverReceivesAddMsgsWhenObservablerAdded) {
+	Observable<Msg> observable1(Msg(1, true), Msg(1, false, true));
+	Observable<Msg> observable2(Msg(2, true), Msg(2, false, true));
+	Observabler<Msg> observabler(Msg(0, true), Msg(0, false, true));
+	ObserverMock observer(MsgSeq{
+			Msg(1, true),
+			Msg(2, true),
+			});
+	observable1.addObserver(observabler);
+	observable2.addObserver(observabler);
+
+	observabler.addObserver(observer);
+}
+
+TEST(Observabler, ObservablerSendsNoAddMsg) {
+	Observabler<Msg> observabler(Msg(0, true), Msg(0, false, true));
+	// observer is declared later, therefore dies before observabler
+	ObserverMock observer(MsgSeq{
+			});
+
+	observabler.addObserver(observer);
+}
+
+TEST(Observabler, ObservablerSendsNoRemMsg) {
+	ObserverMock observer(MsgSeq{
+			});
+	// observabler is declared later, therefore dies before observer
+	Observabler<Msg> observabler;
+
+	observabler.addObserver(observer);
+}
+
+TEST(Observabler, ObserverReceivesRmMsgsWhenObservablerRemoved) {
+	Observable<Msg> observable1(Msg(1, true), Msg(1, false, true));
+	Observable<Msg> observable2(Msg(2, true), Msg(2, false, true));
+	ObserverMock observer(MsgSeq{
+			Msg(1, true),
+			Msg(2, true),
+			Msg(1, false, true),
+			Msg(2, false, true),
+			});
+
+	{
+		Observabler<Msg> observabler;
+		observable1.addObserver(observabler);
+		observable2.addObserver(observabler);
+
+		observabler.addObserver(observer);
+	}
+}
+
+TEST(Observabler, MoveConstruction) {
+	Observabler<Msg> observabler1;
+	Observable<Msg> observable1(Msg(1, true), Msg(1, false, true));
+	Observable<Msg> observable2(Msg(2, true), Msg(2, false, true));
+	ObserverMock observer(MsgSeq{
+			Msg(1, true),
+			Msg(2, true),
+			Msg(1, false, true),
+			Msg(2, false, true),
+			});
+	observable1.addObserver(observabler1);
+	observable2.addObserver(observabler1);
+	{
+		Observabler<Msg> observabler2(std::move(observabler1));
+		observabler2.addObserver(observer);
+	}
+}
+
+TEST(Observabler, weirdBS) {
+	Observabler<Msg> observabler1;
+	Observable<Msg> observable1(Msg(1, true), Msg(1, false, true));
+	Observable<Msg> observable2(Msg(2, true), Msg(2, false, true));
+	observable1.addObserver(observabler1);
+	observable2.addObserver(observabler1);
+}
+
+/*TODO test observabler:
+ * copy and move constructors / assignments
+ * realloc
+ * proper destruction - valgrind
+ */
