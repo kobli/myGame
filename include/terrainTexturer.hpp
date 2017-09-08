61df197dd70564db2e6b57982ce2f60b782e3410
Ales Koblizek <koblizek.ales@seznam.cz>
2017-09-08 21:00:51 +0200

    use only perlin for texture blending

--- include/terrainTexturer.hpp
+++ include/terrainTexturer.hpp
@@ -35,19 +35,24 @@ class TerrainTexturer {
 			float d2 = textureLevels[1].second.distanceFrom(z);
 			float dd = std::abs(d1-d2);
 
-			static std::random_device rd;
-			static std::mt19937 gen(rd());
-			std::uniform_real_distribution<> dis(d1, (d2-d1)/dd+d1-0.5);
-			float blend = dis(gen);
-			static const float ps = 5;
-			static Perlin p(ps+1,ps+1);
-			blend *= (1.5+p.val(x*ps, y*ps))/3;
+			static auto none = SAVEIMAGE("texture_blend.bmp", [](unsigned x, unsigned y){ return blendCoef(x/1000.,y/1000.)*255; }, vec2u(1000));
+			float blend = blendCoef(x,y);
 			
-			//return SColor(255, textureLevels[0].first, textureLevels[1].first, (1-blend)*255);
-			return video::SColor(255, textureLevels[0].first, textureLevels[1].first, (1-std::max(0.f, std::min(1.f,2*p.val(x*ps, y*ps))))*255);
+			return video::SColor(255, textureLevels[0].first, textureLevels[1].first, blend*255);
 		}
 
 	private:
+		static float blendCoef(f32 x, f32 y) 
+		{
+			static const float ps = 5;
+			static Perlin p(ps+1,ps+1);
+			float b = p.val(x*ps, y*ps);
+			b *= 4;
+			b = min(1.f, max(-1.f, b));
+			b = (b+1)/2;
+			return b;
+		}
+
 		// returns 1 for horizontal normal, 0 for vertical normal
 		static inline float slope(vec3f normal) 
 		{
